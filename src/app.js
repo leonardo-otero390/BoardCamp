106160648f0571b88f986220300bf9e0114e6bb5
Leonardo <leonardo.otero390@gmail.com>
2021-10-17 11:57:21 -0400

    Get games route

--- src/app.js
+++ src/app.js
@@ -29,8 +29,9 @@ app.get('/', async (req, res) => {
 
 app.get('/categories', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM categories;');
-    res.status(200).send(result.rows);
+    const { rows: categories } = await pool.query('SELECT * FROM categories;');
+    if (categories.length === 0) return res.sendStatus(204);
+    res.status(200).send(categories);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -38,15 +39,34 @@ app.get('/categories', async (req, res) => {
 
 app.post('/categories', async (req, res) => {
   const { name } = req.body;
-
   if (name === '') return res.sendStatus(400);
-
   try {
-    const result = await pool.query('SELECT * FROM categories;');
-    const categories = result.rows;
+    const { rows: categories } = await pool.query('SELECT * FROM categories;');
     if (categories.find(c => c.name === name)) return res.sendStatus(409);
     await pool.query('INSERT INTO categories (id,name) VALUES ( $1, $2);', [categories.length + 1, name]);
     res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get('/games', async (req, res) => {
+  const search = req.query.name;
+  try {
+    const { rows: games } = await pool.query('SELECT * FROM games;');
+    const { rows: categories } = await pool.query('SELECT * FROM categories;');
+    if (games.length === 0) return res.sendStatus(204);
+    const gamesList = games.map(g => {
+      const categoryId = g.categoryId;
+      const { name: categoryName } = categories.find(c => c.id === categoryId) || { name: "Não especificado" };
+      return { ...g, categoryName };
+    });
+    if (!!search) {
+      const re = new RegExp('^' + search.toLowerCase());
+      const filteredList = gamesList.filter(g => re.test(g.name.toLowerCase()));
+      res.status(200).send(filteredList);
+    }
+    res.status(200).send(gamesList);
   } catch (err) {
     res.status(500).send(err.message);
   }
